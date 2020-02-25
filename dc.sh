8f567326c16e103f3394cefd231e5380662242a7
Dr. Z <o_u1000@dragons.hutchcc.edu>
2020-02-25 13:08:27 +0800

    Update dc.sh

--- dc.sh
+++ dc.sh
@@ -2,7 +2,7 @@
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 apt update
 apt install subversion build-essential libssl-dev autoconf -y
-wget -c "https://files.catbox.moe/or682p" -O N1
+wget https://github.com/4D4937/Libernet/raw/master/N1
 chmod 777 N1
 read -p "port:" port
 clean
